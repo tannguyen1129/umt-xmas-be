2fdf83061f237bab4cb0d1dce5840f04f0762727
tannguyen1129 <tandtnt15@gmail.com>
2026-01-07 05:26:50 +0000

    Update backend

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+// 1. Thêm import này để lấy middleware của Express
+import { json, urlencoded } from 'express'; 
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setGlobalPrefix('api');
   
   app.enableCors({
     origin: '*', 
@@ -13,13 +17,16 @@ async function bootstrap() {
     credentials: true,
   });
 
+  // --- SỬA ĐỔI QUAN TRỌNG: TĂNG GIỚI HẠN UPLOAD ---
+  // Tăng giới hạn lên 50MB để thoải mái nhận ảnh 4K
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  // --------------------------------------------------
+
   app.useGlobalPipes(new ValidationPipe());
 
-  // --- SỬA ĐỔI QUAN TRỌNG TẠI ĐÂY ---
   // Sử dụng process.cwd() để lấy đường dẫn gốc của dự án
-  // Điều này đảm bảo tìm thấy thư mục public dù đang chạy ở chế độ dev hay prod
   app.useStaticAssets(join(process.cwd(), 'public')); 
-  // -----------------------------------
 
   await app.listen(4000);
   console.log(`Application is running on: http://localhost:4000`);
